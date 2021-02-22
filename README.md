447885beb54a7b4f0f3cfb1d26517f0cc04e0792
Ardra Ashok <ardraasok@yahoo.in>
2021-02-22 17:33:33 +1100

    Added read me

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CakeShopCart
+This cake shop menu organiser is used to sort and see different categories of the cake sorting 
+
+**Technologies used**
+
+## HTML
+## CSS
+## JavaScript
+
+
+
+<a href="https://myworkspiya.github.io/CakeShopCart/">Demo Link</a>
--- script/main.js
+++ script/main.js
@@ -209,3 +209,12 @@ function displayButtons() {
     });
   });
 }
+
+/* Function for showing the cart */
+(function (){
+  const cartInfo = document.getElementById('cart-info');
+  const cart = document.getElementById('cart');
+  cartInfo.addEventListener('click',function(){
+    cart.classList.toggle('show-cart');
+  })
+})();
